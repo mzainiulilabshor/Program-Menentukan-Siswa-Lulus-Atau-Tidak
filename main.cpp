68383cbe9e42261df33b6e1d0d559117cf2542ba
mzainiulilabshor <45529539+mzainiulilabshor@users.noreply.github.com>
2018-12-09 15:05:52 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int nilai;
+cout << " Silahkan Masukan Nilai Yang Anda Peroleh ";cin >> nilai;
+if (nilai>=75){
+    cout << " Selamat Anda Lulus ";
+}
+else {
+    cout << " Maaf Anda Belum LULUS ";
+}
+    return 0;
+}
